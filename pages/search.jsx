dc2e4fa0409d5a8c6c20818a3e08d2cb403488f1
parthjoshi2403200524032005 <139873563+parthjoshi2403200524032005@users.noreply.github.com>
2024-08-27 19:25:15 +0530

    Add-new-pages

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useRouter } from "next/router"; // Import useRouter from next/router
 
-const hello = () => {
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const router = useRouter(); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query) {
+      router.push(`/searchresults/${query}`); 
+    }
+  };
+
   return (
-    <div>
-      Next-hello
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Enter search query"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
-export default hello
+export default Search;
